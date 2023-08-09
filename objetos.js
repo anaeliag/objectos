b09058554e5851b27027664939261d19d7855e92
Ana Garcia <sailor_ana@hotmail.com>
2023-08-08 19:19:51 -0600

    adding files

---
+++ objetos.js
@@ -0,0 +1,27 @@
+/*var john = {
+	firstName: 'John',
+	lastName: 'Doe',
+	birthYear: 1990
+}
+
+console.log(john.firstName);	// 'John'
+
+john.firstName = 'Jane';
+
+console.log(john.firstName);	// 'Jane'
+
+john['firstName'] = 'Joe';
+
+console.log(john['firstName']);	// 'Joe'
+
+var john = {
+	firstName: 'John',
+	lastName: 'Doe',
+	birthYear: 1990,
+	calculateAge: function() {
+		var today = new Date();
+		var year = today.getFullYear();
+
+		this.age = year - this.birthYear;
+	}
+}*/
